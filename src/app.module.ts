3e9b90d1308270ba565e90463cc9eba1372b9073
Sandu Luca <s.luca@unifun.com>
2021-10-28 10:00:14 +0300

    implemented the `i18n` module for Nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,22 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { WebhookModule } from './webhook/webhook.module';
 import configuration from './config/configuration';
+import * as path from 'path';
+import { I18nModule, I18nJsonParser } from 'nestjs-i18n';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [configuration],
       isGlobal: true
+    }),
+    I18nModule.forRoot({
+      fallbackLanguage: 'en_US',
+      parser: I18nJsonParser,
+      parserOptions: {
+        path: path.join(__dirname, '/i18n/'),
+        watch: process.env.NODE_ENV === 'production',
+      },
     }),
     WebhookModule,
   ],
